b4ba6f2982b3c47aaba1bfe4e9745d2305d06bf8
sitnarf <sitnarf@gmail.com>
2024-08-07 14:34:44 +0200

    minor update

--- hcve_lib/transforms.py
+++ hcve_lib/transforms.py
@@ -1,3 +1,35 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+import miceforest as mf
+
+
+# noinspection PyUnusedLocal
+class MiceForest(BaseEstimator, TransformerMixin):
+    def __init__(self, iterations=1, random_state=None):
+        self.iterations = iterations
+        self.random_state = random_state
+        self.kds = None
+
+    def fit(self, X, y=None):
+        self.kds = mf.ImputationKernel(
+            X,
+            random_state=self.random_state,
+            data_subset=0.5,
+            train_nonmissing=True,
+            mean_match_candidates={
+                24: 0,
+                26: 0
+            },
+        )
+        self.kds.mice(
+            self.iterations,
+            n_estimators=100,
+        )
+        return self
+
+    def transform(self, X, y=None):
+        return self.kds.impute_new_data(X).complete_data(0)
+
+
 from optuna import Trial
 from pandas import DataFrame
 from sklearn.base import TransformerMixin, BaseEstimator

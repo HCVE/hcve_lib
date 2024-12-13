a9ca31e3fdf377e05d93afac327c2ed3d53473ad
sitnarf <sitnarf@gmail.com>
2024-12-13 15:06:56 +0100

    refactoring

--- hcve_lib/stats.py
+++ hcve_lib/stats.py
@@ -2,6 +2,7 @@ from math import sqrt
 from numbers import Real
 from typing import List, Tuple
 
+import numpy as np
 from numpy import std, mean
 from scipy.stats import t
 
@@ -10,11 +11,15 @@ def confidence_interval(
     sample_values: List[Real],
     degrees_of_freedom: int = None,
 ) -> Tuple[float, Tuple[float, float], float]:
-    degrees_of_freedom = degrees_of_freedom if degrees_of_freedom is not None else len(
-        sample_values) - 1
+    degrees_of_freedom = (
+        degrees_of_freedom if degrees_of_freedom is not None else len(sample_values) - 1
+    )
     sample_mean: float = mean(sample_values)  # type: ignore
+
+    # type: ignore
+    # noinspection PyTypeChecker
     sample_standard_deviation = std(
-        sample_values,  # type: ignore
+        sample_values,
         ddof=len(sample_values) - degrees_of_freedom,
     )
     standard_error = sample_standard_deviation / sqrt(degrees_of_freedom)
@@ -28,5 +33,5 @@ def confidence_interval(
             scale=standard_error,
         )
     except Exception:
-        interval = (0., 0.)
+        interval = (np.nan, np.nan)
     return sample_mean, interval, sample_standard_deviation
